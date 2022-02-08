ad60ac58a2e22de207321ebff029b4528290a0f1
seokzin <seokzin95@gmail.com>
2022-02-08 14:05:01 +0900

    feat: Init vite project

---
+++ src/GlassCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GlassCard = () => {
+  return <div>Glass Card</div>;
+};
+
+export default GlassCard;
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import GlassCard from "./GlassCard";
+
+ReactDOM.render(<GlassCard />, document.getElementById("root"));
